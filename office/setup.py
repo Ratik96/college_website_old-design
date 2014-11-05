83374e90e3a69f9ab6fd2f5d1f9fec1064559606
theSage21 <arjoonn.94@gmail.com>
2014-11-05 08:52:24 +0530

    finalized ratik laptop fix

--- office/setup.py
+++ office/setup.py
@@ -140,15 +140,8 @@ def faculty():
 		department=office.models.department()
 		department.name=dept.strip().replace('_',' ')
 		nick=clean_to_string(dept.strip().replace('_','').replace(' ','').replace('/','').lower())[:5]
-<<<<<<< HEAD
-		while nick in nicks_already_used:
-			print nick
-			nick=nick[:len(nick)-1]
-=======
 		if nick in nicks_already_used:
-			nick = 'PHY'
-			#nick=''.join(random.sample(nick, len(nick))
->>>>>>> 05709d585260366a57922aed4034ec26b535eb18
+			nick = 'phy'
 		department.nickname=nick
 		nicks_already_used.append(nick)
 		department.save()
