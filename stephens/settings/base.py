3eb3fdb180b616e729351af4272dd3392d01e1db
theSage21 <arjoonn.94@gmail.com>
2014-12-02 13:36:55 +0530

    Added and committed to work with pythonanywhere

--- stephens/settings/base.py
+++ stephens/settings/base.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -22,7 +22,7 @@ SECRET_KEY = '_f6w7c)tdm)=%cidz6)2kmi7+k=+6o65$d5zi9eiz_$l($blsl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -75,14 +75,13 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = None
 
-USE_I18N = False#Translation
+USE_I18N = False
 
-USE_L10N = False#Localization
+USE_L10N = False
 
 USE_TZ = False
 
@@ -91,15 +90,12 @@ USE_ETAGS=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 MEDIA_URL='/media/'
-#===================================================================
-MEDIA_ROOT='/Users/ratik/Development/django/college_site/'
-STATIC_ROOT='/Users/ratik/Development/django/college_site/static'
-if os.environ['USER']=='ghost':
-	MEDIA_ROOT='/home/'+os.environ['USER']+'/Documents/college_site/'
-	STATIC_ROOT='/home/'+os.environ['USER']+'/Documents/college_site/static'
-	
-#===================================================================
 STATIC_URL = '/static/'
+#===================================================================
+MEDIA_ROOT=os.path.join(BASE_DIR,'mediafiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+#===================================================================
+
 
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL='/login'
