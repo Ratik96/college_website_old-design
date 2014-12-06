6fb2f10a13b49b8ca11dc08f680d48f9bb66e329
theSage21 <arjoonn.94@gmail.com>
2014-12-06 17:32:09 +0530

    Completed the provision of captcha text.

--- captcha/models.py
+++ captcha/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.forms import ModelForm
+def hash_table(models.Model):
+	'''Table to store hashes and their answers'''
+	string=models.CharField(max_length=5)
+	hash=models.CharField(max_length=32)
--- captcha/views.py
+++ captcha/views.py
@@ -2,25 +2,43 @@ from django.http import HttpResponse
 import json
 import random
 import hashlib
+import captcha
 
 
 def get_captcha(request):
-	'''Returns a captcha string along with a hash'''
+	'''Returns a captcha string along with an answer hash.'''
 	data={}
-	#
-	x=random.choice(range(50))
-	y=random.choice(range(50))
-	op=random.choice(['+','-','x','>','<'])
-	data['string']=string(x)+op+string(y)
-	answer=eval(data['string'])
-	net=data['string']+string(answer)
-	#
-	hash=hashlib.md5(net)
-	data['hash']=hash.hexdigest()
+	if random.random()<0.5:#create a new captcha string and hash
+		#
+		x=random.choice(range(100))
+		y=random.choice(range(100))
+		op=random.choice(['+','-','x','>','<'])
+		data['string']=string(x)+op+string(y)
+		#see if it already exists
+		try:
+			cap=captcha.models.hash_table.objects.get(string=data['string'])
+		except:#if does not exist
+			answer=eval(data['string'])#find answer
+			#
+			q_hash=hashlib.md5(data['string'])
+			a_hash=hashlib.md5(string(answer))
+			#create a new entry
+			new_hash=captcha.models.hash_table()
+			new_hash.string=data['string']
+			new_hash.hash=a_hash.hexdigest()
+			new_hash.save()
+			data['hash']=new_hash.hash
+		else:
+			data['hash']=cap.hash
+	else:#return an existing captcha
+		cap=captcha.models.hash_table.objects.all()
+		cap=random.choice(cap)
+		data['string']=cap.string
+		data['hash']=cap.hash
 	return HttpResponse(json.dumps(data),content_type='application/json')
+	
 def check_captcha(request):
 	'''Returns a captcha submission and thus validates the humanity of the user'''
-	
 	return HttpResponse(json.dumps(True),content_type='application/json')
 	return HttpResponse(json.dumps(False),content_type='application/json')
 	
